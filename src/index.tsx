81f890d9e232c4eb14aa428932be72dceb3d1795
Imran Ali <iali1@ualberta.ca>
2021-07-20 00:16:33 -0600

    Added firebase and moved app to components. Removed default unused css

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import RealmApp from './providers/Realm'
 
 ReactDOM.render(
   <React.StrictMode>
-    <RealmApp>
-      <App />
-    </RealmApp>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
